ca537555606f4a40353c8f73d23d1555ef27b18c
Saurabh <saurabh.av@cube26.com>
2016-02-09 17:22:56 +0530

    updated production settings

--- smokeager/settings_production.py
+++ smokeager/settings_production.py
@@ -1,16 +1,3 @@
-import keyring
-import getpass
-
-database_name = 'smokeager'
-username = 'aliasav'
-password = keyring.get_password(database_name, username)
-
-while password == None :
-    password = getpass.getpass(database_name + " Password:\n")
-    # store the password
-    keyring.set_password(database_name, username, password)
-
-
 DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
@@ -18,11 +5,13 @@ ALLOWED_HOSTS = ["*"]
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': database_name,                       # Or path to database file if using sqlite3.
-        'USER': username,                      # Not used with sqlite3.
-        'PASSWORD': password,                  # Not used with sqlite3.
+        'NAME': 'smokeager',                       # Or path to database file if using sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': 'autobottransform',                  # Not used with sqlite3.
         'HOST': 'localhost',                         # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '3306',
 
     }
 }
+
+print "Settings production included!"
