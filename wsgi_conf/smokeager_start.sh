0b59295002df1e4805c928d350842644603b1946
aliasav <saurabhav.torres@gmail.com>
2016-03-02 15:02:53 +0530

    modified wsgi startup script

--- wsgi_conf/smokeager_start.sh
+++ wsgi_conf/smokeager_start.sh
@@ -2,6 +2,8 @@
 
 # !! DONOT run as ROOT !!
 
-uwsgi /home/aliasav/workspace/smokeager/wsgi_conf/smokeager_server.ini 
+#uwsgi /home/aliasav/workspace/smokeager/wsgi_conf/smokeager_server.ini
+uwsgi --http :8000 --chdir /home/aliasav/workspace/smokeager/ --wsgi-file smokeager/wsgi.py --master --pidfile=/tmp/uwsgi.pid\
+--processes=1 --vacuum --daemonize=/tmp/uwsgi_smokeager_daemon
 
 #ini file should be in the same folder from where this script is being run
